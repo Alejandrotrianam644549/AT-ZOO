e927eb1fb78085639d64c223ed4bf13209fef2fc
Alejandrotrianam644549 <alejandrotrianamedina@gmail.com>
2024-10-31 09:09:34 -0500

    hol

---
+++ src/Perro.java
@@ -0,0 +1,11 @@
+// Clase Perro que extiende Animal
+class Perro extends Animal {
+    public Perro(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println("El perro ladra");
+    }
+}
